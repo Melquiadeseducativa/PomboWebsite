afc70c52fe24d5393916c95258b26fa2286fb163
Martin Reyes <55515152+mreyes2000@users.noreply.github.com>
2020-09-19 14:28:39 -0500

    Changed keys

--- PomboContactFB.js
+++ PomboContactFB.js
@@ -1,9 +1,9 @@
 var firebaseConfig = {
     apiKey: "AIzaSyC6LtdxQSZEjWd6p02JAlkABkacJKx4WyE",
-    authDomain: "contactform-93f91.firebaseapp.com",
-    databaseURL: "https://contactform-93f91.firebaseio.com",
-    projectId: "contactform-93f91",
-    storageBucket: "contactform-93f91.appspot.com",
+    authDomain: "https://contactform-42df0.firebaseio.com/",
+    databaseURL: "https://contactform-42df0.firebaseio.com",
+    projectId: "contactform-42df0",
+    storageBucket: "contactform-42df0.appspot.com",
     messagingSenderId: "232680294518",
     appId: "1:232680294518:web:15c8c749321d0f53dfb757",
     measurementId: "G-35L2G2C2LN"
